4c26426bb4742e24d8671159a8ec8965316c07d4
Пономарёв Андрей <damned01@yandex.ru>
2022-02-27 23:59:41 +0500

    step2

--- main.py
+++ main.py
@@ -8,12 +8,33 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and (course in self.courses_in_progress or course in self.finished_courses):
+            if course in lecturer.courses_attached:
+                lecturer.grades[course] += [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
 
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+
+    def __init__(self, name, surname):
+        self.grades = {}
+        super().__init__(name, surname)
+
+
+class Reviewer(Mentor):
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -23,18 +44,3 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-class Lecturer(Mentor):
-
-    def __init__(self, name, surname, gender):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-
-class Reviewer(Mentor):
-
-    def __init__(self, name, surname, gender):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
